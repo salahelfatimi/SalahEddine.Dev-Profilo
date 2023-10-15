005df9d4b41fe317b9c99ffbcb4b740798224961
ELFATIMI SALAHEDDINE <salahfatimi16@gmail.com>
2023-10-15 18:30:42 +0100

    add

--- src/components/About.js
+++ src/components/About.js
@@ -21,7 +21,7 @@ export default function About(){
                     <div className=' text-black flex flex-col gap-8'>
                         <div className='flex flex-col gap-4 text-center lg:text-start'>
                             <span className={`text-[#148BFB] font-extrabold text-xl ${PaytoneOne.className}`}>ABOUT ME</span>
-                            <span className={` font-bold `} >Hello, I'm Salaheddine Elfatimi, a Junior Full Stack Developer residing in Marrakech, Morocco. 📍</span>
+                            <span className={` font-bold `} >Hello, I&apos;m Salaheddine Elfatimi, a Junior Full Stack Developer residing in Marrakech, Morocco. 📍</span>
                         </div>
                         
                         <span className={` text-center lg:text-start ${VarelaRound.className}`}>I have a solid foundation in Frontend technologies, including HTML, CSS, React, Vite, JavaScript, Redux, Tailwind, Bootstrap, and Next.js, allowing me to craft engaging and responsive user interfaces. On the backend, I have experience with Php, Sql, Laravel, and MongoDB, enabling me to develop functional and efficient applications. Proficient in design using Figma and Canva, I ensure that the user experience is both aesthetically pleasing and intuitive. Additionally, I am well-versed in utilizing essential software development tools like Git and GitHub to enhance collaboration and version control in projects.</span>
