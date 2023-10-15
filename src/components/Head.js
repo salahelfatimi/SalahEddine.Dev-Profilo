ab3640388fe1991c1bacb66d9a599fbfcd639d20
ELFATIMI SALAHEDDINE <salahfatimi16@gmail.com>
2023-10-15 17:25:13 +0100

    fix issue of tech task

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,7 +1,6 @@
 "use client"
 import Image from 'next/image'
-import { FrontEnd,BackEnd,Designe } from '@/json/language'
-import { useState } from 'react'
+import TechStack from './Tech-Stack'
 
 async function   Head(){
     return (
@@ -27,68 +26,7 @@ async function   Head(){
 
                             </div>
                         </div>
-                        <div className="space-y-12">
-                            <div className="text-center font-mono">
-                                <span className=" font-extrabold text-3xl text-black border-b-4 border-black">Tech Stack</span>
-                            </div>
-                            <div className="  grid grid-cols-1 lg:grid-cols-2  gap-8 ">                         
-                                <div className=" space-y-6  text-center">
-                                    <span className="text-black font-bold  text-2xl">Front-End </span>
-                                    <div className=" grid grid-cols-1 lg:grid-cols-1 gap-4">
-                                    {
-                                        FrontEnd.map((res,index)=>(
-                                            <div key={index} className='flex items-center gap-4'>
-                                                <Image  className=""   src={res.languageImage} width={50} height={50} alt={res.languageName} />
-                                                <div className={`rounded-md w-[100%]   p-4 ` }>
-                                                <div className="w-full bg-gray-200 rounded-full h-2.5 ">
-                                                    <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: `${res.languageProgress}% `, transition: 'width 0.5s ease' }}></div>
-                                                </div>                                                    
-                                                </div>  
-                                            </div>   
-                                        ))                                       
-                                    }                                   
-                                    </div>
-                                </div>
-                                <div className="space-y-6 text-center">
-                                    <span className="text-black font-bold  text-2xl">Back-End </span>
-                                    <div className=" grid grid-cols-1 lg:grid-cols-1 gap-4">
-                                    {
-                                        BackEnd.map((res,index)=>(
-                                            <div key={index} className='flex items-center gap-4'>
-                                                <Image  className=" "   src={res.languageImage} width={50} height={50} alt={res.languageName} />
-                                                <div className={`rounded-md w-[100%]   p-4 ` }>
-                                                <div className="w-full bg-gray-200 rounded-full h-2.5 ">
-                                                    <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: `${res.languageProgress}% `, transition: 'width 0.5s ease' }}></div>
-                                                </div>
-                                                    
-                                                </div>  
-                                            </div>   
-                                        ))    
-                                    }                                                                 
-                                    </div>
-                                    <div className="space-y-6 text-center">
-                                    <span className="text-black font-bold  text-2xl">Designe</span>
-                                    <div className=" grid grid-cols-1 lg:grid-cols-1 gap-4">
-                                    {
-                                        Designe.map((res,index)=>(
-                                            <div key={index} className='flex items-center gap-4'>
-                                                <Image  className=" "   src={res.languageImage} width={50} height={50} alt={res.languageName} />
-                                                <div className={`rounded-md w-[100%]   p-4 ` }>
-                                                <div className="w-full bg-gray-200 rounded-full h-2.5 ">
-                                                    <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: `${res.languageProgress}% `, transition: 'width 0.5s ease' }}></div>
-                                                </div>
-                                                    
-                                                </div>  
-                                            </div>   
-                                        ))                                  
-                                    }                                                                     
-                                    </div>
-                                </div>
-                                </div>
-                                
-                            </div>
-                            
-                        </div>
+                        <TechStack/> 
                     </div>
                 </div>
             </div>
---
+++ src/components/Tech-Stack.js
@@ -0,0 +1,72 @@
+"use client"
+import { FrontEnd,BackEnd,Designe } from '@/json/language'
+import Image from 'next/image'
+
+export default function TechStack(){
+    return(
+        <>
+        <div className="space-y-12">
+                            <div className="text-center font-mono">
+                                <span className=" font-extrabold text-3xl text-black border-b-4 border-black">Tech Stack</span>
+                            </div>
+                            <div className="  grid grid-cols-1 lg:grid-cols-2  gap-8 ">                         
+                                <div className=" space-y-6  text-center">
+                                    <span className="text-black font-bold  text-2xl">Front-End </span>
+                                    <div className=" grid grid-cols-1 lg:grid-cols-1 gap-4">
+                                    {
+                                        FrontEnd.map((res,index)=>(
+                                            <div key={index} className='flex items-center gap-4'>
+                                                <Image  className=""   src={res.languageImage} width={50} height={50} alt={res.languageName} />
+                                                <div className={`rounded-md w-[100%]   p-4 ` }>
+                                                <div className="w-full bg-gray-200 rounded-full h-2.5 ">
+                                                    <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: `${res.languageProgress}% `, transition: 'width 0.5s ease' }}></div>
+                                                </div>                                                    
+                                                </div>  
+                                            </div>   
+                                        ))                                       
+                                    }                                   
+                                    </div>
+                                </div>
+                                <div className="space-y-6 text-center">
+                                    <span className="text-black font-bold  text-2xl">Back-End </span>
+                                    <div className=" grid grid-cols-1 lg:grid-cols-1 gap-4">
+                                    {
+                                        BackEnd.map((res,index)=>(
+                                            <div key={index} className='flex items-center gap-4'>
+                                                <Image  className=" "   src={res.languageImage} width={50} height={50} alt={res.languageName} />
+                                                <div className={`rounded-md w-[100%]   p-4 ` }>
+                                                <div className="w-full bg-gray-200 rounded-full h-2.5 ">
+                                                    <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: `${res.languageProgress}% `, transition: 'width 0.5s ease' }}></div>
+                                                </div>
+                                                    
+                                                </div>  
+                                            </div>   
+                                        ))    
+                                    }                                                                 
+                                    </div>
+                                    <div className="space-y-6 text-center">
+                                    <span className="text-black font-bold  text-2xl">Designe</span>
+                                    <div className=" grid grid-cols-1 lg:grid-cols-1 gap-4">
+                                    {
+                                        Designe.map((res,index)=>(
+                                            <div key={index} className='flex items-center gap-4'>
+                                                <Image  className=" "   src={res.languageImage} width={50} height={50} alt={res.languageName} />
+                                                <div className={`rounded-md w-[100%]   p-4 ` }>
+                                                <div className="w-full bg-gray-200 rounded-full h-2.5 ">
+                                                    <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: `${res.languageProgress}% `, transition: 'width 0.5s ease' }}></div>
+                                                </div>
+                                                    
+                                                </div>  
+                                            </div>   
+                                        ))                                  
+                                    }                                                                     
+                                    </div>
+                                </div>
+                                </div>
+                                
+                            </div>
+                            
+                        </div>
+        </>
+    )
+}
