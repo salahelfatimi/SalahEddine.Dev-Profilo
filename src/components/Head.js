56dca3ed835e277f039d8053144ad1e2befe9d95
salahelfatimi <salahfatimi16@gmail.com>
2023-11-01 23:31:09 +0100

    add image

--- src/components/Head.js
+++ src/components/Head.js
@@ -38,7 +38,7 @@ import TechStack from './Tech-Stack'
                                
                             </div>
                             <div>
-                                <Image className="   rounded-full" src="/profile3.png" width={200} height={200} alt="profile" />
+                                <Image className=" shadow-2xl  rounded-full" src="/profile4.png" width={250} height={250} alt="profile" />
 
                             </div>
                         </div>
