22ffde9b7f25301aa1a5246839cdec6b528c1b40
salahelfatimi <salahfatimi16@gmail.com>
2023-11-05 19:06:39 +0100

    add transition for nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -76,7 +76,7 @@ export default function Nav (){
                     </div>
                     
                 
-                <div className={`fixed  inset-0 flex items-center justify-center bg-white dark:bg-[#2d2e32]  ${open==true?"block":"hidden"}`}>
+                <div className={`fixed  inset-0 flex items-center justify-center bg-white dark:bg-[#2d2e32] duration-700 transition  ${open==true?"translate-x-0":"translate-x-full"}`}>
              
                     <button onClick={()=>{setOpen(!open)}} className="absolute top-4 right-4 dark:text-[#bb86fc] text-gray-500 hover:text-gray-700">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
