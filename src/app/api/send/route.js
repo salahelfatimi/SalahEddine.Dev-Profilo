91217fe2e4e8b304da2dd81c6e32f57173718c0a
salahelfatimi <salahfatimi16@gmail.com>
2023-11-05 23:57:30 +0100

    add

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,7 +2,7 @@ import { EmailTemplate } from '@/components/EmailTemplate';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_WPdBBYuu_FeYrroRyi34TSUbcMJ388QDr");
 
 export async function POST(request) {
   try {
